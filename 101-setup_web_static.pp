d3b614306ff29b641a947fa523921fcf584d2654
LilyPr <lilyeginald09@gmail.com>
2023-01-25 20:02:34 +0300

    add change

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,6 @@
 # a puppet version of out setup of nginx for web static
-exec { '/usr/bin/env apt-get -y update' : }
--> exec { '/usr/bin/env apt-get -y install nginx' : }
+exec { '/usr/bin/env sudo apt-get -y update' : }
+-> exec { '/usr/bin/env sudo apt-get -y install nginx' : }
 -> exec { '/usr/bin/env sed -i "/listen \[::\]:80 default_server/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default' : }
 -> exec { '/usr/bin/env sed -i "/listen \[::\]:80 default_server/ a\\\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default' : }
 -> exec { '/usr/bin/env sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default' : }
